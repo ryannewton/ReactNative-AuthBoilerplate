fdfcc6bba241adefbe0d914e50167250cb3e80c3
Ryan Newton <newton1988@gmail.com>
2017-01-22 17:26:58 -0500

    Adds Header to root component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 'use strict';
 
+// Import libraries
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+
+//Import components, functions, and styles
+import { Header } from './components/common';
 
 class App extends Component {
 	render() {
 		return (
 			<View>
+				<Header headerText="Authentication" />
 				<Text>Placeholder text</Text>
 			</View>
 		);
