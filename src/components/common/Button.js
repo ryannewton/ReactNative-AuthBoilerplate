5f6f861e928db8b376cf25708f75af075d73113b
Ryan Newton <newton1988@gmail.com>
2017-01-22 17:08:30 -0500

    Adds 'use-strict' across files

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import React from 'react';
 import { Text, View } from 'react-native';
 
