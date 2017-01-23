5744e2f4ab67944d69ac7c0ca3a4bb60bbb8fbaa
Ryan Newton <newton1988@gmail.com>
2017-01-23 02:00:44 -0500

    Adds server call for Log in attempt

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,16 +2,37 @@
 
 // Import libraries
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 //Import components, functions, and styles
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-	state = { email: '', password: '' };
+	state = { email: '', password: '', error: '' };
+
+	onButtonPress() {
+		// User pressed 'Log in'
+		const { email, password } = this.state;
+
+		// Clear previous error message
+		this.setState({ error: '' });
+
+		// 1. Check if credentials match an existing account
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				// 2. If doesn't match existing accounts, try creating an account
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						// 3. If the account cannot be signed into or created (e.g. email in use), show an error
+						this.setState({ error: 'Authentication Failed' });
+					});
+			});
+	}
 
 	render() {
 		return (
 			<Card>
+				{/*Input field for email*/}
 				<CardSection>
 					<Input
 						placeholder="john@gmail.com"
@@ -21,6 +42,7 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				{/*Input field for password*/}
 				<CardSection>
 					<Input
 						secureTextEntry
@@ -31,8 +53,9 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 
+				{/*Log in button*/}
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log in
 					</Button>
 				</CardSection>
