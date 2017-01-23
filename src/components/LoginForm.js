dcf419082850e43ef7a20e070477857286717cec
Ryan Newton <newton1988@gmail.com>
2017-01-22 21:27:19 -0500

    Adds password Input to LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,7 +7,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-	state = { email: '' };
+	state = { email: '', password: '' };
 
 	render() {
 		return (
@@ -20,7 +20,16 @@ class LoginForm extends Component {
 						onChangeText={(email) => this.setState({ email })}
 					/>
 				</CardSection>
-				<CardSection />
+
+				<CardSection>
+					<Input
+						secureTextEntry
+						placeholder="password"
+						label="Password"
+						value={this.state.password}
+						onChangeText={(password) => this.setState({ password })}
+					/>
+				</CardSection>
 
 				<CardSection>
 					<Button>
