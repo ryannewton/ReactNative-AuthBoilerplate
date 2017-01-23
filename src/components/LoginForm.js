65c7c61556ca7db8a843d4370cdefb995f035864
Ryan Newton <newton1988@gmail.com>
2017-01-22 20:08:30 -0500

    Adds scaffolding to LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,10 +4,22 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
+//Import components, functions, and styles
+import { Button, Card, CardSection } from './common';
+
 class LoginForm extends Component {
 	render() {
 		return (
-			<View />
+			<Card>
+				<CardSection />
+				<CardSection />
+
+				<CardSection>
+					<Button>
+						Log in
+					</Button>
+				</CardSection>
+			</Card>
 		);
 	}
 }
