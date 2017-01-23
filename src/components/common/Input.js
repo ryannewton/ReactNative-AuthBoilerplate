8d2d8f32e7fdac2e9a18114de8980368cfa30bb8
Ryan Newton <newton1988@gmail.com>
2017-01-22 21:12:12 -0500

    Adds props and structure to Input component

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,11 +2,20 @@
 
 // Import libraries
 import React from 'react';
-import { View } from 'react-native';
+import { TextInput, View, Text } from 'react-native';
 
-const Input = () => {
+const Input = ({ label, value, onChangeText, placeholder }) => {
 	return (
-		<View />
+		<View>
+			<Text>{label}</Text>
+			<TextInput
+				placeholder={placeholder}
+				autoCorrect={false}
+				style={{ height: 50, width: 100 }}
+				value={value}
+				onChangeText={onChangeText}
+			/>
+		</View>
 	);
 };
 
